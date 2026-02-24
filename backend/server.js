4a6504507c0f582dd5d37622382b6c7bed71eec9
Ubuntu <ubuntu@ip-172-31-47-70.ap-south-1.compute.internal>
2026-02-24 18:20:15 +0000

    new

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+
 const express = require("express");
 //const cors = require("cors");
-
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
+app.use(express.json());
 // parse requests of content-type - application/json
 app.use(express.json());
 
